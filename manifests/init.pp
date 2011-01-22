2e79c64861b7e99e032c58518ee10028ddc2239f
Zoran Zaric <zz@zoranzaric.de>
2011-01-22 05:45:55 +0100

    Add user and group handling

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,26 +21,53 @@
 
 class vhosts {
 
-	define vhost($vhost, $domain){
+	define vhost($vhost, $domain, $vhost_user=false, $vhost_group=false){
+		if $vhost_user {
+			$user = $vhost_user
+		} else {
+			$user = "${vhost}.${domain}"
+		}
+
+		if $vhost_group {
+			$group = $vhost_group
+		} else {
+			$group = "${vhost}.${domain}"
+		}
+
+		user{"$user":
+			ensure => present,
+			home => "/var/www/${domain}/${vhost}/",
+		}
+
+		group{"$group":
+			ensure => present,
+		}
+
 		# this is a weird workaround that is needed because recursive creation of
 		# direcotries isn't supported (mkdir -p)
 		if defined(File["/var/www/${domain}/"]) {
 			file{"/var/www/${domain}/${vhost}/":
-				ensure => directory
+				ensure => directory,
+				owner => "${user}",
+				group => "${group}",
 			}
 		} else {
 			file{"/var/www/${domain}/":
-				ensure => directory
+				ensure => directory,
 			}
 			file{"/var/www/${domain}/${vhost}/":
-				ensure => directory
+				ensure => directory,
+				owner => "${user}",
+				group => "${group}",
 			}
 		}
 		file{"/var/www/${domain}/${vhost}/htdocs/":
-			ensure => directory
+			ensure => directory,
+			owner => "${user}",
+			group => "${group}",
 		}
 		file{"/var/www/${domain}/${vhost}/logs/":
-			ensure => directory
+			ensure => directory,
 		}
 	}
 }
